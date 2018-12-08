db3fdfa530995a75867116a7a7a06a91a1c6832a
AsmundH <aasmund.haugse@gmail.com>
2018-12-08 17:55:41 +0100

    Add packages necessary for babel transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ export default config = {
      rules: [{
          test: /\.(js|jsx)$/,
          exclude: /node_modules/,
-         use: ['babel-loader']
+         use: {
+            loader: 'babel-loader',
+            options: {
+               presets: ['@babel/preset-env']
+            }
+         }
       },
       {
          test: /(\.css|\.scss|\.sass)$/,
