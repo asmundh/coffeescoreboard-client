9abe48494acaa760a869ff239c82e2627d7abeda
AsmundH <aasmund.haugse@gmail.com>
2019-02-03 15:44:17 +0100

    Remove unnecessary webpack options.

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
-const env = process.env.NODE_ENV;
-
 module.exports = {
-  entry: {
-    app: './src/App.js',
-  },
-  mode: env || 'development',
+  mode: 'development',
+  entry: './src/App.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+  module:{
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
-    },
-    {
-      test: /(\.css|\.scss|\.sass)$/,
-      use: [{
-        loader: 'style-loader',
-      }, {
-        loader: 'css-loader',
-      }, {
-        loader: 'sass-loader',
-      }],
-    }],
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        use: [
+          {
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }
+      ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'index.html'
     }),
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
   ],
-};
+}
