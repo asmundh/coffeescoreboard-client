421cf1d2a4a60148a914a2988fd83a5a755ab5f0
asmundh <aasmund.haugse@gmail.com>
2020-10-09 11:19:37 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
+      favicon: './public/favicon.png'
     }),
     new CleanWebpackPlugin(['dist']),
   ],
