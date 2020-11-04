501b46598531aafd4003b9202d6f9213beed3a78
asmundh <aasmund.haugse@gmail.com>
2020-11-04 16:51:45 +0100

    Add new brew modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import './Modal.css';
-import { string } from 'prop-types';
-import QRCodeWrapper from './QRCodeWrapper';
-
-
-const generateLink = (rfidToLink) => {
-  console.log(rfidToLink);
-  return `http://localhost:8089/register/${rfidToLink}`;
-};
 
 
 const Modal = (props) => {
-  const { rfid } = props;
+  const { children } = props;
   return (
     <div className="overlay">
-      <div className="inner-modal">
-        <p className="modal-text">Scan koden</p>
-        <QRCodeWrapper urlToLink={generateLink(rfid)} />
-        <br />
-        <p className="modal-text">Eller gå inn på</p>
-        <a className="modal-link" href={`http://localhost:8089/register/${rfid}`}>{`http://localhost:8089/register/${rfid}`}</a>
-      </div>
+      {children}
     </div>
   );
 };
-
-Modal.propTypes = {
-  rfid: string.isRequired,
-};
-
 
 export default Modal;
